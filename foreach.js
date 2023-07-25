5c6315fe7f8524732e1f1e0f929342d29b817ba7
Shiyam4896 <133642510+Shiyam4896@users.noreply.github.com>
2023-07-25 12:12:38 +0530

    Add files via upload
    The solution for the foreach session from the Udemy's ES6 course

---
+++ foreach.js
@@ -0,0 +1,12 @@
+function handlePosts() {
+    var posts = [
+      { id: 23, title: 'Daily JS News' },
+      { id: 52, title: 'Code Refactor City' },
+      { id: 105, title: 'The Brightest Ruby' }
+    ];
+    posts.foreach(function(post){
+        handlePosts(post);
+     } 
+     );    
+    
+}
