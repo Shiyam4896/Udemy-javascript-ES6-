9c4412f370cfff30149c9b0ecbcd303504dbf3d9
Shiyam4896 <133642510+Shiyam4896@users.noreply.github.com>
2023-07-25 12:46:46 +0530

    Create maphelper(1).js
    The code show the solution for the map helper exercise from the Udemy's maphelper session .

---
+++ maphelper(1).js
@@ -0,0 +1,12 @@
+var images = [
+    { height: '34px', width: '39px' },
+    { height: '54px', width: '19px' },
+    { height: '83px', width: '75px' },
+  ];
+  
+  var heights = images.map(function(high)
+  {
+    return high.height;
+  }
+  );
+  
