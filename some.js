0d6d516e3f2a032c2c0fb5172c6dcf9d0a80735e
Shiyam4896 <133642510+Shiyam4896@users.noreply.github.com>
2023-07-28 08:47:57 +0530

    Create some.js
    The solution for the "some" problem from Udemy's  ES6 course.

---
+++ some.js
@@ -0,0 +1,9 @@
+var requests = [
+  { url: '/photos', status: 'complete' },
+  { url: '/albums', status: 'pending' },
+  { url: '/users', status: 'failed' },
+];
+
+var inProgress = requests.some(function (request) {
+  return request.status === 'pending';
+});
